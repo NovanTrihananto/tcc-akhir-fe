343cee1a0a3864898cccc14fa4e2106d8badf807
Farizal <123220199@student.upnyk.ac.id>
2025-06-11 15:29:49 +0700

    add api

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://betcc-700231807331.us-central1.run.app", // sesuaikan dengan backend kamu
+  baseURL: "http://localhost:5000", // sesuaikan dengan backend kamu
 });
 
 export default API;
